291429e07f273616d84448f123de716dc086f09c
jenshaw86 <bloo.j.shaw@gmail.com>
2019-08-08 11:17:46 -0700

    Add users#show action

--- relationships-tracker-api/app/controllers/users_controller.rb
+++ relationships-tracker-api/app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
     def show
+        user = User.find(params[:id])
+        render :json => user, include: [:relationships, :events]
     end
 
     def update
