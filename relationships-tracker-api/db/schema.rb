a66bcf1d9ed4e11be5932828b447610bfe707f90
jenshaw86 <bloo.j.shaw@gmail.com>
2019-08-09 08:58:54 -0700

    Convert to PostgreSQL

--- relationships-tracker-api/db/schema.rb
+++ relationships-tracker-api/db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_08_07_204203) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "events", force: :cascade do |t|
     t.string "name"
     t.string "start_date"
