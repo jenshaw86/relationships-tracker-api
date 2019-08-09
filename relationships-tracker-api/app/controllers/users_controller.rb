8b071f24ee497acc8ea362f07b27f089a6d70be4
jenshaw86 <bloo.j.shaw@gmail.com>
2019-08-08 20:10:21 -0700

    Add index controller action to User

--- relationships-tracker-api/app/controllers/users_controller.rb
+++ relationships-tracker-api/app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
 class UsersController < ApplicationController
+    def index
+        users = User.all
+        render :json => users, include: [:relationships, :events]
+    end
+
     def show
         user = User.find(params[:id])
-        render :json => user
+        render :json => user, include: [:relationships, :events]
     end
 
     def update
