e033b1c3ad6f495fd62342ee6e18905a4ad2c2d7
jenshaw86 <bloo.j.shaw@gmail.com>
2019-08-11 20:00:54 -0700

    Add RelationshipEvent index, create controller actions

--- relationships-tracker-api/app/controllers/relationship_events_controller.rb
+++ relationships-tracker-api/app/controllers/relationship_events_controller.rb
@@ -1,2 +1,20 @@
 class RelationshipEventsController < ApplicationController
+  def index
+    relationshipEvents = RelationshipEvent.all
+    render :json => relationshipEvents
+  end
+
+  def create
+    relationshipEvent = RelationshipEvent.new(relationship_event_params)
+    relationshipEvent.save
+    render :json => relationshipEvent
+  end
+
+  private 
+    def relationship_event_params
+        params.require(:relationship_event).permit(
+            :relationship_id, 
+            :event_id
+        )
+    end
 end
--- relationships-tracker-api/config/routes.rb
+++ relationships-tracker-api/config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :events
   resources :relationships
   resources :users
+  resources :relationship_events
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
