c9375f19d1cf0ae1a7c5ccc0bf8e0d0e6f2a8ae1
jenshaw86 <bloo.j.shaw@gmail.com>
2019-09-20 15:53:59 -0700

    Set up for JWT and user auth

--- relationships-tracker-api/app/controllers/application_controller.rb
+++ relationships-tracker-api/app/controllers/application_controller.rb
@@ -1,2 +1,36 @@
 class ApplicationController < ActionController::API
+  
+  def issue_token(user) # returns a JWT
+    JWT.encode(user, 'mustLurvd0gz')
+  end
+
+  def decoded_token(token) # returns the user
+    if token
+      begin
+        JWT.decode(token, 'mustLurvd0gz', true, {:algorithm => 'HS256'}) # returns [payload (user_id), algo]
+      rescue JWT::DecodeError
+        nil
+      end
+    end
+  end
+
+  def token
+    request.headers['Authorization'].split(' ')[1] # takes index 1 because header looks like this => Authorization: 'Bearer <token>'
+  end
+
+  def user_id 
+    if decoded_token
+      decoded_token[0][user_id] # returns current user's id
+    end
+  end
+
+  def current_user
+    @user ||= User.find_by(id: user_id) # if @user doesn't already have a value, it is assigned an instance
+  end
+
+  def logged_in?
+    !!current_user # does current_user have a value? is user logged in?
+  end
+
+
 end
--- relationships-tracker-api/db/schema.rb
+++ relationships-tracker-api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_07_204203) do
+ActiveRecord::Schema.define(version: 2019_09_20_001902) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 2019_08_07_204203) do
     t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
