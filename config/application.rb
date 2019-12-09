7e96135b0a019e0280112f2b19a5590fa46afe6b
jenshaw86 <bloo.j.shaw@gmail.com>
2019-12-08 20:20:38 -0800

    Change CORS allow origins header

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ module RelationshipsTrackerApi
   class Application < Rails::Application
     Rails.application.config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins '*'
+        origins 'https://jenshaw86.github.io/'
     
         resource '*',
           headers: :any,
