fb83b8cf6c7d2b5dc5fbede72dfd26865b898970
jenshaw86 <bloo.j.shaw@gmail.com>
2019-12-08 20:44:39 -0800

    Modify production.rb for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,6 +22,11 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
+  config.public_file_server.headers = {
+    # 'Access-Control-Allow-Origin' => '*',  # only for debugging
+    'Access-Control-Allow-Origin' => 'https://jenshaw86.github.io'
+    # 'Access-Control-Request-Method' => %w{GET OPTIONS}.join(",")
+  }
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
