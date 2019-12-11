e94419ed37ee505a1c3a685a8972b15737fc5bf7
jenshaw86 <bloo.j.shaw@gmail.com>
2019-12-10 17:06:00 -0800

    CORS stuff

--- config/application.rb
+++ config/application.rb
@@ -19,14 +19,13 @@ Bundler.require(*Rails.groups)
 
 module RelationshipsTrackerApi
   class Application < Rails::Application
-
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
     
         resource '*',
           headers: :any,
-          methods: [:get, :post, :put, :patch, :delete, :options, :head]
+          methods: [:get, :post, :patch, :delete]
       end
     end
     
