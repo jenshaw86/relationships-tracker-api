cd456ba34f32a71c0a4611f176c5d23d101c784c
jenshaw86 <bloo.j.shaw@gmail.com>
2019-12-12 13:29:10 -0800

    experimenting with cors

--- config/application.rb
+++ config/application.rb
@@ -19,15 +19,15 @@ Bundler.require(*Rails.groups)
 
 module RelationshipsTrackerApi
   class Application < Rails::Application
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins '*'
+    # config.middleware.insert_before 0, Rack::Cors do
+    #   allow do
+    #     origins '*'
     
-        resource '*',
-          headers: :any,
-          methods: [:get, :post, :patch, :delete]
-      end
-    end
+    #     resource '*',
+    #       headers: :any,
+    #       methods: [:get, :post, :patch, :delete]
+    #   end
+    # end
     
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
